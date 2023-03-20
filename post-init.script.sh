a9664788a01db587dbf5146e4602cd9232f1398c
Viet Nguyen <47852365+PeterVNguyen@users.noreply.github.com>
2023-03-20 09:20:26 +0700

    update template

--- post-init.script.sh
+++ post-init.script.sh
@@ -3,15 +3,15 @@
 echo "\nInitializing git repository\n"
 git init
 
-echo "\nInitializing husky (Please hit return/enter key for the next prompt)\n"
-npx husky-init
+# echo "\nInitializing husky (Please hit return/enter key for the next prompt)\n"
+# npx husky-init
 
-echo "\nInstalling node_modules\n"
-npm install
+# echo "\nInstalling node_modules\n"
+# npm install
 
-echo "\nInitializing husky hooks\n"
-npx husky set .husky/pre-commit "npm run test && npm run lint"
+# echo "\nInitializing husky hooks\n"
+# npx husky set .husky/pre-commit "npm run test && npm run lint"
 
 mv _env .env
-mv _env.production .env.production
-mv _env.staging .env.staging
+# mv _env.production .env.production
+# mv _env.staging .env.staging
